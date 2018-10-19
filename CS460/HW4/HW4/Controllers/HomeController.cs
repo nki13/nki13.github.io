5ab9fa89239c934c9946428525d2728f6f9c1019
nki13 <nki13@wou.edu>
2018-10-19 14:15:03 -0700

    Got mile converter working properly

--- CS460/HW4/HW4/Controllers/HomeController.cs
+++ CS460/HW4/HW4/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace HW4.Controllers
 {
@@ -30,7 +31,41 @@ namespace HW4.Controllers
         [HttpGet]
         public ActionResult Converter()
         {
+            double input = Convert.ToDouble(Request.QueryString["input"]);
+            string unit = Request.QueryString["unit"];
+            // I want the Query String values
+            Debug.WriteLine(input);
+            Debug.WriteLine(unit);
+
+            double output = 0;
+            ViewBag.result = false;
+            if (unit == "millimeters")
+            {
+                output = input * 1609344;
+                ViewBag.result = true;
+            }
+            else if (unit == "centimeters")
+            {
+                output = input * 160934.4;
+                ViewBag.result = true;
+            }
+            else if (unit == "meters")
+            {
+                output = input * 1609.344;
+                ViewBag.result = true;
+            }
+            else if (unit == "kilometers")
+            {
+                output = input * 1.609344;
+                ViewBag.result = true;
+            }
+
+            string message = "The conversion is: " + Convert.ToString(output) + " " + unit;
+
+            ViewBag.Message = message;
+
             return View();
         }
+
     }
 }
