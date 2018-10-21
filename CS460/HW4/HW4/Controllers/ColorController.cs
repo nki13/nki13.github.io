3cc8030da0dee116cd9234e69e598f191e786faf
nki13 <nki13@wou.edu>
2018-10-21 12:22:59 -0700

    Added checking for valid result values to be under 255

--- CS460/HW4/HW4/Controllers/ColorController.cs
+++ CS460/HW4/HW4/Controllers/ColorController.cs
@@ -32,14 +32,49 @@ namespace HW4.Controllers
             Color color2 = ColorTranslator.FromHtml(c2);
 
             // Actual mixing of colors to placeholders for results
-            int alpha = color1.A + color2.A;
-            int red = color1.R + color2.R;
-            int green = color1.G + color2.G;
-            int blue = color1.B + color2.B;
+            int alpha = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            // Added checking to make sure values of alpha, red, green, and blue are valid
+            if (color1.A + color2.A >= 255)
+            {
+                alpha = 255;
+            }
+            else
+            {
+                alpha = color1.A + color2.A;
+            }
+            if (color1.R + color2.R >= 255)
+            {
+                red = 255;
+            }
+            else
+            {
+                red = color1.R + color2.R;
+            }
+            if (color1.G + color2.G >= 255)
+            {
+                green = 255;
+            }
+            else
+            {
+                green = color1.G + color2.G;
+            }
+            if (color1.B + color2.B >= 255)
+            {
+                blue = 255;
+            }
+            else
+            {
+                blue = color1.B + color2.B;
+            }
 
             // Translation back to hex from Color type for showing results
             string result = ColorTranslator.ToHtml(Color.FromArgb(alpha, red, green, blue));
 
+            //Results
             string message = "Your mixed color is: " + result;
 
             ViewBag.Message = message;
